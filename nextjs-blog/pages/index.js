216aabae2c6e763e0610297cd8b9fd0d8d5f0bae
Zach Chernenko <zach@chernenko.com>
2022-01-15 14:29:47 -0800

    UBC page

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.reddit.com/r/leagueoflegends/"
           target="_blank"
           rel="noopener noreferrer"
         >
