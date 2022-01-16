81688318c8c424b6f5ef22cc31b9eee603198c99
hsaito18 <hsaito2002@gmail.com>
2022-01-16 10:32:07 -0800

    fixes

--- pages/posts/UBC.js
+++ pages/posts/UBC.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import bgi from '/public/main mall.jpg'
 import Image from 'next/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '/styles/schools.module.css'
 import ubcStyles from '/styles/ubc.module.css'
-import { getSortedPostsData } from '../../lib/posts'
+import { getSortedPostsData } from '../../lib/ubc_posts'
 
 
-=======
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { getSortedPostsData } from "../../lib/ubc_posts";
-import bgi from "/public/main mall.jpg";
-import styles from "/styles/schools.module.css";
-import ubcStyles from "/styles/ubc.module.css";
->>>>>>> 7f7d9635926f408097715551ae006cbb24bad731
 
 export async function getServerSideProps(context) {
   const allPostsData = getSortedPostsData()
@@ -43,16 +33,6 @@ export default function UBC({ allPostsData }) {
 
         <div>
           <h1 className={styles.title}>University of British Columbia</h1>
-<<<<<<< HEAD
-          <a href="new_post" className={ubcStyles.btn}>Ask a Question</a>
-
-        <div id = 'cock'>
-          <h1>Posts</h1>
-          {allPostsData.map(({ id, title, author, content}) => (
-      
-              <Link href={`/posts/${id}`}>
-                <a className = {ubcStyles.single}>
-=======
           <a href="new_post_ubc" className={ubcStyles.btn}>
             Create Post
           </a>
@@ -62,7 +42,6 @@ export default function UBC({ allPostsData }) {
             {allPostsData.map(({ id, title, author, content }) => (
               <Link href={`/posts/ubcid/${id}`}>
                 <a className={ubcStyles.single}>
->>>>>>> 7f7d9635926f408097715551ae006cbb24bad731
                   <h3>{title}</h3>
                   <h4>{author}</h4>
                   <br></br>
