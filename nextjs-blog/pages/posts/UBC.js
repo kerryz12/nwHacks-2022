9b1c0c1cc64c414f005d5d9621dd7fa00cec6772
hsaito18 <hsaito2002@gmail.com>
2022-01-15 16:02:16 -0800

    undid ubc

--- nextjs-blog/pages/posts/UBC.js
+++ nextjs-blog/pages/posts/UBC.js
@@ -9,22 +9,6 @@ export default function UBC() {
             <a>Back to home</a>
           </Link>
         </h2>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              {id}
-              <br />
-              <Date dateString={date} />
-            </li>
-          ))}
-        </ul>
-      </section>
       </>
     )
   }
